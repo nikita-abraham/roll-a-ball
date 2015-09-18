1d7a4dd22b39ff51a8509815a38f3c986bb6e395
Nikita Abraham <nikitaa1995@gmail.com>
2015-09-17 21:29:56 -0400

    Roll-A-Ball finished

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start()
+	{
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
+		SetCountText ();
+		winText.text = "";
+	
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		
+		GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win!";
+		}
+	}
+}
+
